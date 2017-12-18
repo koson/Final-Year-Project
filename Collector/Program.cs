7ff6492d65c495f87edaa753cc57d698b6b846d1
up737044 <up737044@myport.ac.uk>
2017-12-18 16:43:14 +0000

    Added configuration file. added Sensor class.

--- Collector/Program.cs
+++ Collector/Program.cs
@@ -6,17 +6,35 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
 using System.IO;
+using System.Xml.Serialization;
 
 namespace Collector
 {
     class Program
     {
+        [XmlArray("Sensors")]
+        [XmlArrayItem("Sensors")]
+        private List<Sensor> sensors = new List<Sensor>();
         static void Main(string[] args)
         {
-            int port = 1500;
-            String slaveAddress = "192.168.0.74";
-            TcpClient client = new TcpClient(slaveAddress, port);
-            NetworkStream nwStream = client.GetStream();
+            Program program = new Program();
+            program.ReadConfig();
+            byte[] rawData = program.RequestData(program.sensors[0]);
+            program.PrintData(rawData);
+            Console.Read();
+        }
+
+        void CalculateData(byte[] rawData, int sensorType) { } //calculates data based off voltage received
+
+        void PrintData(byte[] rawData)
+        {
+            for(int i = 0; i < 12; i++)
+            {
+                Console.WriteLine(rawData[i]);
+            }
+        }
+
+        byte[] RequestData(Sensor sensor) { //reads data from a sensor
             byte upperTransIdentifier = 0b0;
             byte transIdentifier = (byte)0;
             byte protocolIdentifier = 0b0;
@@ -24,21 +42,24 @@ namespace Collector
             byte lowerHeaderLength = 0b110;
             byte unitIdentifier = 0b1;
             byte functionCode = 0b11;
-            for(int i=0;i<7;i++){
-                transIdentifier = (byte) i;
-                byte[] request = new byte[] {upperTransIdentifier, transIdentifier, protocolIdentifier, protocolIdentifier, upperHeaderLength, lowerHeaderLength, unitIdentifier, functionCode, 0b0, 0b0, 0b0, 0b1};
-            Console.WriteLine(request.ToString());
-
+            byte register = (byte) sensor.Register;
+            Console.WriteLine("register number is : " + register);
+            TcpClient client = new TcpClient(sensor.Address, sensor.Port);
+            NetworkStream nwStream = client.GetStream();
+            byte[] request = new byte[] { upperTransIdentifier, transIdentifier, protocolIdentifier, protocolIdentifier, upperHeaderLength, lowerHeaderLength, unitIdentifier, functionCode, 0b0, register, 0b0, 0b1 };
             nwStream.Write(request, 0, request.Length);
             byte[] received = new byte[client.ReceiveBufferSize];
             Console.WriteLine(received.Length);
             int bytesRead = nwStream.Read(received, 0, client.ReceiveBufferSize);
-            for(int ii=0; ii<11; ii++){
-                Console.WriteLine(received[ii]);
-}
-}
             client.Close();
-            Console.Read();
+            return received;
         }
-    }
+        void ReadConfig() { //reads configuration from a file
+            XmlSerializer configReader = new XmlSerializer(typeof(List<Sensor>));
+            FileStream loadStream = new FileStream("C:\\Users\\Raife\\source\\repos\\Final-Year-Project\\Collector\\sensors.xml", FileMode.Open, FileAccess.Read);
+            sensors = (List<Sensor>)configReader.Deserialize(loadStream);
+            loadStream.Close();
+        } 
+
+    }          
 }
---
+++ Collector/Sensor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Xml.Serialization;
+
+public class Sensor
+{
+    [XmlAttribute("Address")]
+    public string Address { get; set; }
+    [XmlAttribute("port")]
+    public int Port { get; set; }
+    [XmlAttribute("ID")]
+    public int ID { get; set; }
+    [XmlAttribute("SensorType")]
+    public int SensorType { get; set; }
+    [XmlAttribute("ChamberID")]
+    public int ChamberID { get; set; }
+    [XmlAttribute("Register")]
+    public int Register { get; set; }
+
+    public Sensor()
+    {
+
+    }
+
+	public Sensor(String newAddress, int newPort, int newID, int newType, int newChamberID, int newRegister)
+	{
+        Address = newAddress;
+        Port = newPort;
+        ID = newID;
+        SensorType = newType;
+        ChamberID = newChamberID;
+        Register = newRegister;
+	}
+    public Sensor(String newAddress, int newID, int newType, int newChamberID, int newRegister)
+    {
+        Address = newAddress;
+        Port = 502;
+        ID = newID;
+        SensorType = newType;
+        ChamberID = newChamberID;
+        Register = newRegister;
+    }
+}
